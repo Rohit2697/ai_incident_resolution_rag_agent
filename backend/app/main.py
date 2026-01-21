239a5c06490cf2211619974e54ec380732ca2722
Rohit Dey <rohit.dey@ibm.com>
2026-01-21 19:52:37 +0530

    added multiprocessing to support worker

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,24 @@
 from app.server import app
 import uvicorn
+import multiprocessing
+from app.redis.worker.worker import worker
+
+def start_worker():
+  print("Starting worker process")
+  worker.work()
 
 def main():
-  uvicorn.run(app,host="0.0.0.0",port=8080)
-  
-main()
+    print("Starting web server...")
+    uvicorn.run(app,host="0.0.0.0",port=8080)
+    
+if __name__ == "__main__":
+  worker_process = multiprocessing.Process(target=start_worker)
+  worker_process.daemon = True
+  worker_process.start()
+  main()
+
+
+
 
 
 
--- backend/app/redis/worker/worker.py
+++ backend/app/redis/worker/worker.py
@@ -1,4 +1,4 @@
 from rq import SimpleWorker
 from app.redis.cleint.client import queue
 worker=SimpleWorker(queues=[queue])
-worker.work()
+# worker.work()
