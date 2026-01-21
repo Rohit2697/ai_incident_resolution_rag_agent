3f712d8ade556674d34303bfa0d17e9c94c289ef
Rohit Dey <rohit.dey@ibm.com>
2026-01-21 14:16:15 +0530

    fix mongo connection

--- backend/app/db/mongo.py
+++ backend/app/db/mongo.py
@@ -3,24 +3,13 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from app.environment.environment import env_veriables
 
-
-print("Mongo config check:", {
-    "username_present": bool(env_veriables["MONGO_DB"]["username"]),
-    "password_present": bool(env_veriables["MONGO_DB"]["password"]),
-    "host": env_veriables["MONGO_DB"]["host"],
-})
-
 usename=quote_plus(env_veriables["MONGO_DB"]["username"])
 password=quote_plus(env_veriables["MONGO_DB"]["password"]) 
 host=env_veriables["MONGO_DB"]["host"]
 app_name=env_veriables["MONGO_DB"]["app_name"] 
    
 uri = f"mongodb+srv://{usename}:{password}@{host}/?appName={app_name}"
-print("MongoDB URI:", uri)
-
-# Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
-# Send a ping to confirm a successful connection
 try:
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
